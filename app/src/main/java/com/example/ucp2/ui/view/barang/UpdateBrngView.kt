60f25262956e1f440f770423ee3fecb770376693
granityo <frhnspensa@gmai.com>
2024-12-24 14:58:08 +0700

    untuk mengupdate

--- app/src/main/java/com/example/ucp2/ui/view/barang/UpdateBrngView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/barang/UpdateBrngView.kt
@@ -1,2 +1,80 @@
 package com.example.ucp2.ui.view.barang
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.CustomWidget.TopAppBar
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2.ui.viewmodel.barang.UpdateBrngViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UpdateBrgView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateBrngViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val BrgUiState = viewModel.UpdateBrngUiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(BrgUiState.snackbarMessage) {
+        BrgUiState.snackbarMessage?.let { message ->
+            snackbarHostState.showSnackbar(
+                message = message,
+                duration = SnackbarDuration.Long
+            )
+            viewModel.resetSnackbarMessage()
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues)
+                .padding(bottom = 130.dp)
+        ) {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Edit Barang",
+            )
+            InsertBodyBrg(
+                uiState = BrgUiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600) // Delay to allow the update process
+                            withContext(Dispatchers.Main) {
+                                onNavigate() // Navigate after the update
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
