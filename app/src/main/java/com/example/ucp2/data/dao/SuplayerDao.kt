4b13d3e02040e4735dfa13386ccb30ef04f818b0
granityo <frhnspensa@gmai.com>
2024-12-24 14:14:25 +0700

    membuat suplayer DAO untuk mengubungkan entity dan data base

--- app/src/main/java/com/example/ucp2/data/dao/SuplayerDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/SuplayerDao.kt
@@ -3,13 +3,18 @@ package com.example.ucp2.data.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import com.example.ucp2.data.entity.Suplayer
+import com.example.ucp2.entity.Suplayer
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface SuplayerDao {
     @Insert
-    suspend fun insert(suplier: Suplayer)
+    suspend fun insert(suplayer: Suplayer)
 
-    @Query("SELECT * FROM suplayer")
-    suspend fun getAll(): List<Suplayer>
+    @Query("SELECT * FROM suplayer WHERE id = :id")
+    fun getSuplayer(id: Int): Flow<Suplayer>
+
+    @Query("SELECT * FROM suplayer ORDER BY namasuplayer ASC")
+    fun getAllSuplayer(): Flow<List<Suplayer>>
 }
