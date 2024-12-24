41a53c9e87b6a13965bbb831f29b4255ff1b184f
granityo <frhnspensa@gmai.com>
2024-12-24 14:15:59 +0700

    membbuat data base untuk aplikasi

--- app/src/main/java/com/example/ucp2/ui/navigasi/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigasi/AlamatNavigasi.kt
@@ -1,2 +1,43 @@
 package com.example.ucp2.ui.navigasi
 
+interface AlamatNavigasi{
+    val route: String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "Home"
+}
+
+object DestinasiHomeSplyr: AlamatNavigasi {
+    override val route = "HomeSuplayer"
+}
+
+object DestinasiHomeBrng: AlamatNavigasi {
+    override val route = "HomeBarang"
+}
+
+object DestinasiInsertSplyr: AlamatNavigasi {
+    override val route = "Supalyer"
+}
+
+object DestinasiInsertBrng: AlamatNavigasi {
+    override val route = "Barang"
+}
+
+object DestinasiDetailSplyr: AlamatNavigasi {
+    override val route = "Detail"
+    const val id = "id"
+    val routeWithArg = "$route/{$id}"
+}
+
+object DestinasiDetailBrng: AlamatNavigasi {
+    override val route = "detail"
+    const val id = "id"
+    val routeWithArg = "$route/{$id}"
+}
+
+object DestinasiUpdate: AlamatNavigasi {
+    override val route = "update"
+    const val id = "id"
+    val routeWithArg = "$route/{$id}"
+}
